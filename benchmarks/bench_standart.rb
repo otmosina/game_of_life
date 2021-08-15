ffd13eaf44824961523e66cb481ba154791d4f63
otmosina <otmosina@gmail.com>
2021-08-15 23:12:02 +0800

    add saver to benchmarks

--- benchmarks/bench_standart.rb
+++ benchmarks/bench_standart.rb
@@ -9,9 +9,11 @@ population = [
 require_relative '../game.rb'
 require 'ruby-prof'
 RubyProf.measure_mode = RubyProf::WALL_TIME
+s = Universe::Saver.new
 result = Benchmark.realtime do
-  g = Game.new(population: population, printer: Universe::Printer)
+  
+  g = Game.new(population: population, printer: Universe::Printer, saver: s)
   g.loop
 end
-
+puts "#{s.generations_cnt}"
 puts result
