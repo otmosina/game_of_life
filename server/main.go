2f3adad67fb038481adf33e41d21a879d5469eaf
otmosina <otmosina@gmail.com>
2021-09-25 13:14:15 +0800

    add handlers

--- server/main.go
+++ server/main.go
@@ -1,59 +1,57 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"game_of_life/pkg/my.com/me/myproject/mypackage"
-	"game_of_life/pkg/storage"
-	"hello"
+	"io/ioutil"
 	"log"
-	"os"
-	"strconv"
-	"strings"
-	test1 "test1/test11"
-
-	greeting "github.com/headfirstgo/greeting"
-	//greeting "game_of_life/pkg/greeting"
+	"net/http"
 )
 
-// grade, err := strconv.ParseFloat(())
-
-func getFloat() (float64, error) {
-	mypackage.MyFunc()
-	greeting.Hello()
-	filenames, _ := storage.GetAllGenerationFilenames()
-	for _, filename := range filenames {
-		fmt.Println(filename)
-	}
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
+type Page struct {
+	Title string
+	Body  []byte
 }
-func main() {
 
-	hello.Hey()
-	test1.Test1()
-	fmt.Println("Enter a grade:")
-	grade, err := getFloat()
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title
+	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
+	return &Page{Title: title, Body: body}, err
+}
 
-	var status string
-	if grade >= 60 {
-		status = "passing"
-	} else {
-		status = "failing"
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hey, place for %s", r.URL.Path[1:])
+}
 
-	fmt.Println("A grade of", grade, "is", status)
+func h1(text string) string {
+	return "<h1>" + text + "</h1><br>"
+
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	fmt.Println(string(title))
+	page, _ := loadPage(string(title))
+	fullPage := h1(string(page.Title)) + string(page.Body)
+	fmt.Fprint(w, fullPage)
+}
+
+func main() {
+	p1 := &Page{Title: "Game of life", Body: []byte("Here html is located")}
+	p1.save()
+	p2, _ := loadPage("Game of life")
+	fullPage := string(p2.Title) + string(p2.Body)
+	fmt.Println(fullPage)
+
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
